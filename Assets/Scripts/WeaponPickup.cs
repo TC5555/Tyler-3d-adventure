0b61f33338d4194d55f6ee20cbb505560e8a2a40
Tyler Carter <70717266+TC5555@users.noreply.github.com>
2023-05-24 13:12:47 -0700

    pain

--- Assets/Scripts/WeaponPickup.cs
+++ Assets/Scripts/WeaponPickup.cs
@@ -7,16 +7,23 @@ public class WeaponPickup : MonoBehaviour
   
     void OnColliderEnter(Collision collision)
     {
+        
         if (collision.gameObject.tag == "Player")
         {
             //collision.gameObject.GetComponent<PlayerScript>().AddWeapon();
         }
         if (collision.gameObject.tag == "Enemy")
         {
-            transform.GetChild(0).tag = "Enemy";
-            transform.parent.GetComponent<PawnBT>().equipWeapon = transform.GetChild(0).gameObject;
-            transform.GetChild(0).parent = collision.transform.GetChild(0).GetChild(0).GetChild(0).GetChild(0).GetChild(2).GetChild(0).GetChild(0).GetChild(0).GetChild(0);
+        
+            GameObject Weapon = transform.GetChild(0).gameObject;
+            transform.DetachChildren();
+            Weapon.tag = "Enemy";
+            Weapon.transform.SetParent(collision.transform.GetChild(0).GetChild(0).GetChild(0).GetChild(0).GetChild(2).GetChild(0).GetChild(0).GetChild(0).GetChild(0),false);
+            Weapon.transform.parent.GetComponent<PawnBT>().equipWeapon = transform.GetChild(0).gameObject;    
             Destroy(gameObject);
+            Debug.Break();
         }
+       
+
     }
 }
